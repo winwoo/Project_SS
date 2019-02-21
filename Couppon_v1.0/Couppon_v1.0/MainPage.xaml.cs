4e8ecc73460b596b6b3ec38c9b901f4f0c2eff67
SeungA-O <yammy5656@gmail.comm>
2019-02-21 13:44:47 +0900

    Creat Project

---
+++ Couppon_v1.0/Couppon_v1.0/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Couppon_v1._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
